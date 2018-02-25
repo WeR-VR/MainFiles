7cf8857c7ae82f87a9e5f53a72997e96d48ca206
Jatin Kumar <jatin.kumarroxx@hotmail.com>
2018-02-24 21:38:07 -0500

    Some Base Scripts
    Added Gun and Enemy scripts(Empty)...

---
+++ Game Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float Health = 100f;
+    public void takeDam(float amount)
+    {
+        Health -= amount;
+        if (Health<=0f)
+        {
+            Debug.Log("Death");
+        }
+    }
+
+}
---
+++ Game Project/Assets/Scripts/Gun.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    public float damage = 15f;
+    public float range = 100f;
+
+    public Camera fpscam;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+	}
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+      if ( Physics.Raycast(fpscam.transform.position, fpscam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+            if (enemy!=null)
+            {
+                enemy.takeDam(damage);
+            }
+        }
+    }
+}
